efdbc8e63e8e4d860e540fab701ff1c1effe6f49
Federico-Aguirre <sedent333@gmail.com>
2024-07-17 23:46:01 -0300

    some change

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import million from "million/compiler";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build", //this line will tell the build to create a file with this name
 };
 
 export default million.next(nextConfig);
